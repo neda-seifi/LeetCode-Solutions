d9f10c39aba29bc4f995984c4053db5ee5e5ccab
neda-seifi <neda_seifi@yahoo.com>
2025-06-11 00:23:56 -0400

    Update README with Remove Duplicate Occurance from Sorted Array

---
+++ Easy/RemoveElementFromSortedArray.cs
@@ -0,0 +1,19 @@
+namespace LeetCodeSolutions
+{
+    public class RemoveElementSolution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
