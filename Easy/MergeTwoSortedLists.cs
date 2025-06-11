2165bf5d42bfc69a502317f794be8e0447070dc5
neda-seifi <neda_seifi@yahoo.com>
2025-06-10 22:45:00 -0400

    Add merge sorted solution

---
+++ Easy/MergeTwoSortedLists.cs
@@ -0,0 +1,31 @@
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // One of them might still have remaining nodes
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+}
